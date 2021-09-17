d1c9fe8f4030311de73ab8f3ab51aa477ac8a33d
DavidWayneAustinPHP <58784729+DavidWayneAustinPHP@users.noreply.github.com>
2021-09-17 15:19:37 +0100

    Making changes

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum -y install httpd > /var/log/installapache.out 2>&1
-cp -a /tmp/GitHubCodeDeploy/index.html /var/www/html
+cp /tmp/GitHubCodeDeploy/index.html /var/www/html
 
 
--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 service httpd restart > /var/log/restartapache.out 2>&1
+cp /tmp/GitHubCodeDeploy/index.html /var/www/html
