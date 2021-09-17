59de4247a563c14a2180fcf35b129bc67ef6fd32
DavidWayneAustinPHP <58784729+DavidWayneAustinPHP@users.noreply.github.com>
2021-09-17 15:10:54 +0100

    Update installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum -y install httpd > /var/log/installapache.out 2>&1
-sudo cp -a /tmp/GitHubCodeDeploy/index.html /var/www/html
+cp -a /tmp/GitHubCodeDeploy/index.html /var/www/html
 
 
