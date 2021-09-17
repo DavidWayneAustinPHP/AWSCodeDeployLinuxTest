f41ae63116effecc70a9c57de478c6d7a9e9f898
DavidWayneAustinPHP <58784729+DavidWayneAustinPHP@users.noreply.github.com>
2021-09-17 15:05:31 +0100

    Correct script installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 yum -y install httpd > /var/log/installapache.out 2>&1
-sudo cp -a /var/www/html/index.html /tmp/GitHubCodeDeploy
+sudo cp -a /tmp/GitHubCodeDeploy/index.html /var/www/html
+
+
